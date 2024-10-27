3f5d40bac0585a899c6e9dfd9c53cf7eb8aea7e4
ArmandoRosado26 <168446803+ArmandoRosado26@users.noreply.github.com>
2024-10-27 18:16:37 -0500

    script

--- SCRIPT/inversiones.sql
+++ SCRIPT/inversiones.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80030 (8.0.30)
  File Encoding         : 65001
 
- Date: 27/10/2024 16:25:43
+ Date: 27/10/2024 18:15:55
 */
 
 SET NAMES utf8mb4;
@@ -27,19 +27,17 @@ CREATE TABLE `cuentahabiente`  (
   `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `telefono` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id_cuentahabiente`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of cuentahabiente
 -- ----------------------------
-INSERT INTO `cuentahabiente` VALUES (1, 'armando', 'a2r5armando@gmail.com', '9878007434');
-INSERT INTO `cuentahabiente` VALUES (2, 'angel', 'a2r5armando@gmail.com', '9878007434');
-INSERT INTO `cuentahabiente` VALUES (3, 'xd', 'a2r5armando@gmail.com', '9878007434');
-INSERT INTO `cuentahabiente` VALUES (4, 'armandoasdsf', 'a2r5armando@gmail.com', '9878007434');
-INSERT INTO `cuentahabiente` VALUES (5, 'angelfdfweeqw', 'a2r5armando@gmail.com', '3214124');
-INSERT INTO `cuentahabiente` VALUES (6, 'eadasd', 'jose.rosado@seq.edu.mx', '9878007434');
-INSERT INTO `cuentahabiente` VALUES (7, 'eadasdrgadg', 'jose.rosado@seq.edu.mx', '9878007434');
-INSERT INTO `cuentahabiente` VALUES (8, 'angelfdf', 'a2r5armando@gmail.com', '2342');
+INSERT INTO `cuentahabiente` VALUES (1, 'Armando Rosado', 'a2r5armando@gmail.com', '9878007434');
+INSERT INTO `cuentahabiente` VALUES (2, 'Angel', '191k0040@itscarrillopuerto.edu.mx', '9878007323');
+INSERT INTO `cuentahabiente` VALUES (3, 'Alberto', 'a2r5armando@gmail.com', '9878007434');
+INSERT INTO `cuentahabiente` VALUES (4, 'Roger', 'ejemploe@gmail.com', '232424');
+INSERT INTO `cuentahabiente` VALUES (5, 'nuevo', 'ejemploe@gmail.com', '232424323');
+INSERT INTO `cuentahabiente` VALUES (6, 'Ultimo', 'ejemploe@gmail.com', '123223');
 
 -- ----------------------------
 -- Table structure for inversion
@@ -56,18 +54,16 @@ CREATE TABLE `inversion`  (
   CONSTRAINT `inversion_ibfk_1` FOREIGN KEY (`id_cuentahabiente`) REFERENCES `cuentahabiente` (`id_cuentahabiente`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `inversion_chk_1` CHECK (`monto` >= 5000),
   CONSTRAINT `inversion_chk_2` CHECK (`plazo` between 1 and 365)
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of inversion
 -- ----------------------------
-INSERT INTO `inversion` VALUES (1, 1, 56789, 365, 3407.34);
-INSERT INTO `inversion` VALUES (2, 2, 567893000000, 365, 34073600000);
-INSERT INTO `inversion` VALUES (3, 3, 5000, 365, 300);
-INSERT INTO `inversion` VALUES (4, 4, 356776, 365, 21406.6);
-INSERT INTO `inversion` VALUES (5, 5, 421312, 365, 25278.7);
-INSERT INTO `inversion` VALUES (6, 6, 325245, 34, 1817.81);
-INSERT INTO `inversion` VALUES (7, 7, 325245, 34, 1817.81);
-INSERT INTO `inversion` VALUES (8, 8, 21213100, 45, 156919);
+INSERT INTO `inversion` VALUES (1, 1, 5600, 365, 336);
+INSERT INTO `inversion` VALUES (2, 2, 6000, 120, 118.356);
+INSERT INTO `inversion` VALUES (3, 3, 25600000, 365, 1536000);
+INSERT INTO `inversion` VALUES (4, 4, 5000, 362, 297.534);
+INSERT INTO `inversion` VALUES (5, 5, 5000, 365, 300);
+INSERT INTO `inversion` VALUES (6, 6, 62432, 365, 3745.92);
 
 SET FOREIGN_KEY_CHECKS = 1;
